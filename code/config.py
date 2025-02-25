d475d02815da615651050383a44829d995a694d6
高宇岑 <guo_ke@sjtu.edu.cn>
2025-02-25 18:08:15 +0800

    update config

--- code/config.py
+++ code/config.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 import argparse
 
-parser = argparse.ArgumentParser(description="Pytorch implementation of Road-SPDE")
+parser = argparse.ArgumentParser(description="Pytorch implementation of tilde_L1")
 
-# Model
-parser.add_argument('--nof_epoch', default=10000, type=int, help='Number of epochs')
-parser.add_argument('--dimen', default=500, type=int, help='Dimension of embedding')
-parser.add_argument('--batch_size', default=256, type=int, help='Batch size')
-parser.add_argument('--nof_samples', default=3611900, type=int, help='Number of samples')
-parser.add_argument('--learning_rate', default=0.01, type=float, help='learning rate')
-parser.add_argument('--input_dimension', default=2, type=int, help='input dimension')
+# data
 parser.add_argument('--embed_dim', default=128, type=int, help='embedding dimension')
 parser.add_argument('--long_lat_embed_dim', default=2, type=int, help='longtitude and latitude embedding dimension')
+
+# Model
+parser.add_argument('--batch_size', default=256, type=int, help='Batch size')
+parser.add_argument('--num_epoch', default=10000, type=int, help='Number of epochs')
+parser.add_argument('--learning_rate', default=0.01, type=float, help='learning rate')
 
 # Data
 parser.add_argument('--num', default=10, type=int, help='Data number')
@@ -20,7 +19,6 @@ parser.add_argument('--dis_node_num', default=1000, type=int, help='Distance Dat
 parser.add_argument('--output_dimen', default=256, type=int, help='Output dimension')
 parser.add_argument('--tildeL1_ratio', default=0.99, type=int, help='tildeL1 ratio')
 parser.add_argument('--g_training_epochs', default=10, type=int, help='epochs of global training')
-
 
 # NO Direct
 parser.add_argument('--nodirect', default=False, type=bool, help='Whether to use no direct graph')
